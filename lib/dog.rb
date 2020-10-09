565afd60a4f8818e6f2cbb6a87c99bbfc3ec6550
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-09 04:07:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -48,5 +48,12 @@ class Dog
       dog.save
       dog
   end
-
+  def self.new_from_db(row)
+      attributes_hash = {
+        :id => row[0],
+        :name => row[1],
+        :breed => row[2]
+      }
+      self.new(attributes_hash)
+  end
 end
