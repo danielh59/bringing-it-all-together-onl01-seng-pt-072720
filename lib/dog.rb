2a297ad31efa7cc00e97b0123807775a1c7a869f
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-09 04:08:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -48,10 +48,10 @@ class Dog
       dog.save
       dog
   end
-  def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    breed = row[2]
+  def self.new_from_db(doggie_data)
+    id = doggie_data[0]
+    name = doggie_data[1]
+    breed = doggie_data[2]
     dog = self.new(id, name, breed)
     dog
   end
